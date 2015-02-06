ca2981d5d6b638fe1bc5d23426159197b73e07ec
Amadeus Wieczorek <git@amadeusw.com>
2015-02-05 20:46:23 -0800

    Implements IDispoable and creates MemoryMappedFiles

--- src/MemoryMappedQueue.Tests/DataStructureTests.cs
+++ src/MemoryMappedQueue.Tests/DataStructureTests.cs
@@ -7,31 +7,39 @@ namespace CodeConnect.MemoryMappedQueue.Tests
     public class DataStructureTests
     {
         [Theory]
-        [InlineData(16)]
+        [InlineData(8)]
         [InlineData(1 * 1024)]
         [InlineData(20 * 1024)]
         [InlineData(5 * 1024 * 1024)]
         public void InitializeTest(int initialSize)
         {
-            var queue = new MemoryMappedQueue(initialSize);
+            using (var writeQueue = new MemoryMappedQueue(initialSize, true, "test"))
+            {
+                using (var readQueue = new MemoryMappedQueue(initialSize, false, "test"))
+                {
+                    var test = readQueue.GetHashCode();
+                }
+            }
         }
 
         [Fact]
         public void InitializeTooLargeTest()
         {
             var initialSize = int.MaxValue;
-            Assert.Throws(typeof(ArgumentException), () => new MemoryMappedQueue(initialSize));
+            Assert.Throws(typeof(ArgumentException), () => new MemoryMappedQueue(initialSize, true, "test"));
+            Assert.Throws(typeof(ArgumentException), () => new MemoryMappedQueue(initialSize, false, "test"));
         }
 
         [Theory]
-        [InlineData(15)]
+        [InlineData(7)]
         [InlineData(1)]
         [InlineData(0)]
         [InlineData(- 5 * 1024 * 1024)]
         [InlineData(int.MinValue)]
         public void InitializeTooSmallTest(int initialSize)
         {
-            Assert.Throws(typeof(ArgumentException), () => new MemoryMappedQueue(initialSize));
+            Assert.Throws(typeof(ArgumentException), () => new MemoryMappedQueue(initialSize, true, "test"));
+            Assert.Throws(typeof(ArgumentException), () => new MemoryMappedQueue(initialSize, false, "test"));
         }
     }
 }
--- src/MemoryMappedQueue/MemoryMappedQueue.cs
+++ src/MemoryMappedQueue/MemoryMappedQueue.cs
@@ -1,22 +1,29 @@
 ﻿using System;
+using System.IO.MemoryMappedFiles;
 
 namespace CodeConnect.MemoryMappedQueue
 {
-    public class MemoryMappedQueue
+    public class MemoryMappedQueue : IDisposable
     {
         private uint _readPointer, _writePointer;
-        private readonly int _sizeOfPointer;
         private byte[] _data;
+        private MemoryMappedFile _dataFile;
+        private MemoryMappedFile _pointersFile;
+        private bool _writer;
 
         /// <summary>
         /// Creates a circular array that occupies specified amount of space
         /// </summary>
         /// <param name="size">Total size of this data structure</param>
-        public MemoryMappedQueue(int size)
+        public MemoryMappedQueue(int size, bool writer, string name)
         {
             _readPointer = 0;
             _writePointer = 0;
-            _sizeOfPointer = sizeof(int);
+            _writer = writer;
+
+            int sizeOfPointers = 2 * sizeof(int);
+            string dataFileName = name + "_data";
+            string pointersFileName = name + "_pointers";
 
             // Ensure specified size is within bounds (2GB)
             if (size >= int.MaxValue)
@@ -24,13 +31,61 @@ namespace CodeConnect.MemoryMappedQueue
                 throw new ArgumentException("Addressable size is too large.", nameof(size));
             }
             // Ensure that there is enough room for at least one piece of data
-            else if (size < 4 * _sizeOfPointer)
+            else if (size < sizeOfPointers)
             {
                 throw new ArgumentException("Specified size is too small", nameof(size));
             }
 
-            var dataSize = size - 2 * _sizeOfPointer;
-            _data = new byte[dataSize];
+            _data = new byte[size];
+
+            // Access the memory mapped file
+            if (writer)
+            {
+                _dataFile = MemoryMappedFile.CreateNew(dataFileName, size, MemoryMappedFileAccess.ReadWrite);
+                _pointersFile = MemoryMappedFile.CreateNew(pointersFileName, sizeOfPointers, MemoryMappedFileAccess.ReadWrite);
+            }
+            else
+            {
+                _dataFile = MemoryMappedFile.OpenExisting(dataFileName, MemoryMappedFileRights.Read);
+                _pointersFile = MemoryMappedFile.OpenExisting(pointersFileName, MemoryMappedFileRights.Read);
+            }
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _dataFile?.Dispose();
+                    _pointersFile?.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+                _data = null;
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources. 
+        // ~MemoryMappedQueue() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
