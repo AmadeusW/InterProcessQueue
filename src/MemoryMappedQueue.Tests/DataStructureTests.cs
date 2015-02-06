6a283f4337278fe463459f0d2289e984fb30d1ab
Amadeus Wieczorek <git@amadeusw.com>
2015-02-05 19:16:17 -0800

    Creates a solution with library and test projects

---
+++ src/MemoryMappedQueue.Tests/DataStructureTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MemoryMappedQueue.Tests
+{
+    [TestClass]
+    public class DataStructureTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
